01f0a8f99a0fe73e3506ee231d017f56ae52ab16
holden adamec <holden.adamec@gmail.com>
2018-11-08 13:04:30 -0800

    added in joystick inputs

--- src/main/java/org/usfirst/frc/team972/Robot.java
+++ src/main/java/org/usfirst/frc/team972/Robot.java
@@ -5,15 +5,22 @@ import org.usfirst.frc.team972.util.*;
 import edu.wpi.first.wpilibj.*;
 
 public class Robot extends IterativeRobot {
-	static DriveTrain driveTrain;
+	DriveTrain driveTrain;
+	Joystick joystick;
+	
+	final int JOYSTICK_PORT = 0;
 	
 	public void robotInit() {
 		XMLReader reader = new XMLReader("robotConfig");
 		driveTrain = new DriveTrain(reader);
+		joystick = new Joystick(JOYSTICK_PORT);
 	}
 	
 	public void teleopPeriodic() {
-		driveTrain.driveSides(0.5, 0.5);
+		double leftSpeed = joystick.getRawAxis(1);
+		double rightSpeed = joystick.getRawAxis(5);
+		
+		driveTrain.driveSides(leftSpeed, rightSpeed);
 	}
 
 }
